4574060f9d2c7a055d3fadca420b47e08da9fd4d
kaushiknitsil <kaushiknitsil@gmail.com>
2020-05-09 19:18:56 +0530

    Custom arraylist design and calendar

---
+++ src/main/java/com/interview/java/designpatterns/calendarbookingsystem/MyCalendar.java
@@ -0,0 +1,27 @@
+package com.interview.java.designpatterns.calendarbookingsystem;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MyCalendar {
+
+    TreeMap<Integer, Integer> calendar;
+
+    public MyCalendar(){
+        calendar = new TreeMap<>();
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public boolean book(int start, int end){
+        Map.Entry<Integer, Integer> beforeEnd = calendar.lowerEntry(end);
+        if(beforeEnd != null && beforeEnd.getValue() > start){
+            return false;
+        }
+
+        calendar.put(start,end);
+        return true;
+    }
+}
